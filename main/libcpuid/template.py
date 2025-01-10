7aff5e520bfd11aaf8249537fa60a9c55302651e
rietbergenm <mathijs.rietbergen@proton.me>
2025-01-10 15:18:09 +0000

    switch to cmake and add archs

--- main/libcpuid/template.py
+++ main/libcpuid/template.py
@@ -6,11 +6,24 @@ maintainer = "Mathijs Rietbergen <mathijs.rietbergen@proton.me>"
 license = "custom:libcpuid"
 url = "https://github.com/anrieff/libcpuid"
 
-build_style = "configure"
-hostmakedepends = [ "pkgconf" ]
-configure_args = [ "--prefix=/usr" ]
-source = f"{url}/releases/download/v{pkgver}/libcpuid-{pkgver}.tar.gz"
-sha256 = "f5fc4ba4cf19e93e62d3ed130f47045245d682b10506ad29937dacae1bdccc35"
+build_style = "cmake"
+configure_args = [
+    "-DLIBCPUID_ENABLE_TESTS=ON",
+    "-DLIBCPUID_BUILD_DRIVERS=OFF" # only available on arm?
+]
+hostmakedepends = [
+    "cmake",
+    "ninja",
+    "pkgconf",
+    "doxygen"
+]
+checkdepends = [
+    "python"
+]
+make_check_target = "test-old test-fast" # do all test there are
+source = f"{url}/archive/refs/tags/v{pkgver}.tar.gz"
+sha256 = "c54879ea33b68a2e752c20fb0e3cd04439a9177eab23371f709f15a45df43644"
+archs = [ "x86_64", "aarch64" ]
 
 def post_install(self):
     self.install_license("COPYING")
