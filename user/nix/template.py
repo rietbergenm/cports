c3651886870230fc8af99b93702386a360925eab
rietbergenm <mathijs.rietbergen@proton.me>
2025-01-11 13:06:57 +0100

    builds, but illegal instruction all the time

--- user/nix/template.py
+++ user/nix/template.py
@@ -1,22 +1,16 @@
 pkgname = "nix"
 pkgver = "2.25.3"
 pkgrel = 0
-pkgdesc = "Purely functional package manager"
-maintainer = "Mathijs Rietbergen <mathijs.rietbergen@proton.me>"
-license = "LGPL-2.1-or-later"
-url = "https://github.com/NixOS/nix"
-
 build_style = "meson"
-
 configure_args = [
     "--libexecdir=/usr/lib", #XXX: no /usr/libexec
     "-Dnix:profile-dir=/etc/profile.d", # defaults to etc/profile.d which becomes /usr/etc/profile.d with the prefix
     "-Dlibcmd:readline-flavor=readline",
     # next one fixes "ERROR: clang does not know how to do prelinking."
     "-Ddefault_library=shared",
-    "-Ddoc-gen=false",
-    "-Dbindings=false",
-    "-Dunit-tests=false"
+    "-Ddoc-gen=false",   # needs internet
+    "-Dbindings=false",  # we don't need the perl bindings
+    "-Dunit-tests=false" # requires rapidcheck which is not packaged
 ]
 hostmakedepends = [
     "meson",
@@ -26,9 +20,11 @@ hostmakedepends = [
     "pkgconf",
     "bash",
 
-    # doc-gen target (wip)
+    # doc-gen target requires internet because
+    # it uses nix to run stuff and nix needs internet to download
+    # dependecies (it has just been built)
     #"doxygen",
-    #"mdbook", not in cports yet
+    #"mdbook", # packaged myself
 
     # bindings target (wip)
     # generates bindings for perl language
@@ -46,19 +42,24 @@ makedepends = [
     "curl-devel",
     "libseccomp-devel",
     "sqlite-devel",
-    "libgit2-devel", # XXX: should be newer that 1.9.0
     "gc-devel",
-    #"toml11",
     "libedit-readline-devel",
     "lowdown-devel",
-    #"libcpuid-devel" # TODO: check if only for x86_64 or for all
+    "libgit2-devel", # XXX: should be newer that 1.9.0
+    "toml11", # packaged myself
+    # TODO: check if only for x86_64 or for all
+    # seems to only use this library on x86_64
+    #"libcpuid-devel" 
 ]
 checkdepends = [
     # unit-tests target (wip)
     #"rapidcheck", not in cports yet
 ]
 options = [ "!check" ]
-
+pkgdesc = "Purely functional package manager"
+maintainer = "Mathijs Rietbergen <mathijs.rietbergen@proton.me>"
+license = "LGPL-2.1-or-later"
+url = "https://github.com/NixOS/nix"
 source = f"{url}/archive/refs/tags/{pkgver}.tar.gz"
 sha256 = "8d7af0d25371da32783f0b46bce8ff4f0d1dd996db6dee272faf306fcb8e2073"
 
@@ -72,4 +73,24 @@ def post_install(self):
 
 @subpackage("nix-devel")
 def _(self):
+    self.depends = [
+        "pc:nlohmann_json!nlohmann-json",
+        "pc:libgit2!libgit2-devel",
+        "pc:libarchive!libarchive-devel",
+        "pc:libcurl!curl-devel",
+        "pc:libseccomp!libseccomp-devel",
+        "pc:sqlite3!sqlite-devel",
+        "pc:bdw-gc!gc-devel",
+        "pc:libcrypto!openssl-devel",
+        "pc:libsodium!libsodium-devel",
+        "pc:libbrotlicommon!brotli-devel",
+        "pc:libbrotlidec!brotli-devel",
+        "pc:libbrotlienc!brotli-devel",
+        "pc:lowdown!lowdown-devel",
+        #"pc:libcpuid!libcpuid-devel",
+        "libedit-readline-devel", # does not provide pc:readline according to apk, but does have the .pc file
+        "boost-devel",
+        "toml11",
+    ]
+    self.options = [ "!scanrundeps" ]
     return self.default_devel()
