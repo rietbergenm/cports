cd49a6b5c551c2c009b8767a41e3f8df932038ce
rietbergenm <mathijs.rietbergen@proton.me>
2025-01-11 13:50:54 +0100

    still crashing with editline

---
+++ status.md
@@ -0,0 +1,1 @@
+nix is crashing with illegal instructions (both libedit/readline)
--- user/nix/template.py
+++ user/nix/template.py
@@ -5,7 +5,7 @@ build_style = "meson"
 configure_args = [
     "--libexecdir=/usr/lib", #XXX: no /usr/libexec
     "-Dnix:profile-dir=/etc/profile.d", # defaults to etc/profile.d which becomes /usr/etc/profile.d with the prefix
-    "-Dlibcmd:readline-flavor=readline",
+    #"-Dlibcmd:readline-flavor=readline",
     # next one fixes "ERROR: clang does not know how to do prelinking."
     "-Ddefault_library=shared",
     "-Ddoc-gen=false",   # needs internet
@@ -43,10 +43,11 @@ makedepends = [
     "libseccomp-devel",
     "sqlite-devel",
     "gc-devel",
-    "libedit-readline-devel",
+    #"libedit-readline-devel",
     "lowdown-devel",
     "libgit2-devel", # XXX: should be newer that 1.9.0
     "toml11", # packaged myself
+    "editline-devel",
     # TODO: check if only for x86_64 or for all
     # seems to only use this library on x86_64
     #"libcpuid-devel" 
@@ -73,24 +74,26 @@ def post_install(self):
 
 @subpackage("nix-devel")
 def _(self):
+    '''
     self.depends = [
-        "pc:nlohmann_json!nlohmann-json",
+        "pc:nlohmann_json>=3.9!nlohmann-json",
         "pc:libgit2!libgit2-devel",
-        "pc:libarchive!libarchive-devel",
+        "pc:libarchive>=3.1.2!libarchive-devel",
         "pc:libcurl!curl-devel",
-        "pc:libseccomp!libseccomp-devel",
-        "pc:sqlite3!sqlite-devel",
+        "pc:libseccomp>=2.5.5!libseccomp-devel",
+        "pc:sqlite3>=3.6.19!sqlite-devel",
         "pc:bdw-gc!gc-devel",
-        "pc:libcrypto!openssl-devel",
+        "pc:libcrypto>=1.1.1!openssl-devel",
         "pc:libsodium!libsodium-devel",
         "pc:libbrotlicommon!brotli-devel",
         "pc:libbrotlidec!brotli-devel",
         "pc:libbrotlienc!brotli-devel",
-        "pc:lowdown!lowdown-devel",
+        "pc:lowdown>=0.9.0!lowdown-devel",
         #"pc:libcpuid!libcpuid-devel",
         "libedit-readline-devel", # does not provide pc:readline according to apk, but does have the .pc file
-        "boost-devel",
-        "toml11",
+        #"boost-devel",
+        #"toml11",
     ]
     self.options = [ "!scanrundeps" ]
+    '''
     return self.default_devel()
