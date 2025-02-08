3c8cc2ed7111f33c47828d0ff0e834a40c537ed6
Mathijs Rietbergen <mathijs.rietbergen@proton.me>
2025-02-08 10:16:44 +0100

    detect nix path based on settings in nix.conf

--- extra/nix/files/nix.profile.d
+++ extra/nix/files/nix.profile.d
@@ -3,14 +3,31 @@
 if [ -z "$__NIX_SET_ENV_DONE" ]; then
 	. /etc/default/nix
 
+	# default paths
 	DATA_PATH="${XDG_DATA_DIRS-/usr/local/share:/usr/share}"
+	XDG_STATE_HOME="${XDG_STATE_HOME-$HOME/.local/state}"
 
-	if [ "$USE_XDG_BASE_DIRS" == true ] ; then
-		NIX_PREFIX="${XDG_STATE_HOME-$HOME/.local/state}/nix/profile"
-	else
-		NIX_PREFIX="$HOME/.nix-profile"
-	fi
+	case "$USE_XDG_BASE_DIRS" in
+		"true")
+				NIX_PREFIX="$XDG_STATE_HOME/nix/profile"
+			;;
+		"false")
+				NIX_PREFIX="$HOME/.nix-profile"
+			;;
+		*) # "auto" is the default
+		    # use nix command to parse settings so we don't have to
+			case `nix --extra-experimental-features nix-command config show use-xdg-base-directories` in
+				"true")
+					NIX_PREFIX="$XDG_STATE_HOME/nix/profile"
+					;;
+				"false")
+					NIX_PREFIX="$HOME/.nix-profile"
+					;;
+			esac
+			;;
+	esac
 
+	# include the system nix profile as well
 	NIX_BIN_DIRS="$NIX_PREFIX/bin:/nix/var/nix/profiles/default/bin"
 	NIX_DATA_DIRS="$NIX_PREFIX/share:/nix/var/nix/profiles/default/share"
 
@@ -25,4 +42,3 @@ if [ -z "$__NIX_SET_ENV_DONE" ]; then
 	export PATH  XDG_DATA_DIRS
 	export __NIX_SET_ENV_DONE=1
 fi
-
